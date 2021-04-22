b110fbc1e8e603894bc0777b4b4d78e3ee314a91
Rodrigo Guinazu <rodrigoguinazu@outlook.com>
2021-04-22 18:20:33 -0300

    Se creo el comoponente StockCount para agregar o quitar unidades

---
+++ src/components/StockCount/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './stockCount.css'
+
+export default function index({ stock, unidades, addStock }) {
+    return (
+        <>
+            <div className="stock">
+                <button disabled={(stock === 0) || unidades === 1} onClick={ () => addStock(-1)}>-</button>
+                <div>{unidades}</div>
+                <button disabled={stock === 0 || unidades === stock} onClick={ () => addStock(+1)}>+</button>
+            </div>
+            <button disabled={stock === 0} className="add-to-cart">Agregar al Carrito</button>
+        </>
+    )
+}
