7bc512dcae1c7ed72a38d449701180d436f58970
Rodrigo Guinazu <rodrigoguinazu@outlook.com>
2021-05-01 20:46:53 -0300

    Se creo el archivo products con el array de objetos de los producots

---
+++ src/data/products.js
@@ -0,0 +1,136 @@
+const products = [
+    {
+        id: 1,
+        title: "Guitarra Gibson Les Paul 60's",
+        price: 350000,
+        discount: null,
+        brand: "Gibson",
+        category: "Guitarras",
+        video: "CgPpVpbklGk",
+        stock: 2,
+        image: "/images/products/preview_1.jpg"
+    },
+    {
+        id: 2,
+        title: "Bajo Squier Classic Vibe 70's",
+        price: 85000,
+        discount: null,
+        brand: "Squier",
+        category: "Bajos",
+        video: "JXRXLqxtR4c",
+        stock: 3,
+        image: "/images/products/bajo_squier.jpg"
+    },
+    {
+        id: 3,
+        title: "Amplificador Fender Champion 100",
+        price: 70000,
+        discount: null,
+        brand: "Fender",
+        category: "Amplificadores",
+        video: "XKh9M1nfCLY",
+        stock: 0,
+        image: "/images/products/ampli_fender.jpg"
+    },
+    {
+        id: 4,
+        title: "Interfaz de Audio Focusrite 2i2",
+        price: 25000,
+        discount: null,
+        brand: "Focusrite",
+        category: "Grabacion",
+        video: "bku2sGeNVLU",
+        stock: 11,
+        image: "/images/products/focusrite.png"
+    },
+    {
+        id: 5,
+        title: "Guitarra Jackson King V KVXMG",
+        price: 450000,
+        discount: 39000,
+        brand: "Jackson",
+        category: "Guitarras",
+        video: "34tzxbR1Xfc",
+        stock: 1,
+        image: "/images/products/preview.jpg"
+    },
+    {
+        id: 6,
+        title: "Teclado Yamaha YDP-144",
+        price: 70000,
+        discount: 67000,
+        brand: "Yamaha",
+        category: "Teclados",
+        video: "8eiLI3Nx63A",
+        stock: 3,
+        image: "/images/products/yamaha-ydp-144-black.jpg"
+    },
+    {
+        id: 7,
+        title: "Pedal Behringer Ultra Octaver",
+        price: 12000,
+        discount: 10300,
+        brand: "Behringer",
+        category: "Pedales",
+        video: "lb9EejCjdds",
+        stock: 0,
+        image: "/images/products/UO300_P0603_Right_XL.png"
+    },
+    {
+        id: 8,
+        title: "Blackstar Katana 100 MKII",
+        price: 50000,
+        discount: 49000,
+        brand: "Blackstar",
+        category: "Amplificardores",
+        video: "1BhGs0za4cw",
+        stock: 5,
+        image: "/images/products/boss-katana-100-212-mkii.jpg"
+    },
+    {
+        id: 9,
+        title: "Zildijan Planet X 16 Crash",
+        price: 15000,
+        discount: null,
+        brand: "Zildijan",
+        category: "Baterias",
+        video: "QBmcqLYcPuw",
+        stock: 2,
+        image: "/images/products/zildjian-planet-z-16-crash.jpg"
+    },
+    {
+        id: 10,
+        title: "Soporte para Teclado Stagg",
+        price: 7000,
+        discount: null,
+        brand: "Stagg",
+        category: "Accesorios",
+        video: "ExGxpFMtpz0",
+        stock: 13,
+        image: "/images/products/stagg-kxsq5-keyboard-stand.jpg"
+    },
+    {
+        id: 11,
+        title: "Amplificador Marshall DSL40C",
+        price: 96000,
+        discount: null,
+        brand: "Marshall",
+        category: "Amplificadores",
+        video: "FyPa8llaUi8",
+        stock: 3,
+        image: "/images/products/marshall-dsl40cr-opened.jpg"
+    },
+    {
+        id: 12,
+        title: "Parche De Bombo Remo Powerstroke 3",
+        price: 600,
+        discount: null,
+        brand: "Remo",
+        category: "Baterias",
+        video: "bmjTx9ErAsw",
+        stock: 21,
+        image: "/images/products/PARCHE-REMO-POWERSTROKE-3-EBONY-CLEAR.png"
+    }
+]
+
+export default products
